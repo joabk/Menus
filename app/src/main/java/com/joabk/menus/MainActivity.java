449a2f37d093aeabb1ef1c746bf1042931e7bd9d
Joab Karani <mjoab88@gmail.com>
2020-07-05 19:18:14 +0300

    Added ActionMode over Toolbar

--- app/src/main/java/com/joabk/menus/MainActivity.java
+++ app/src/main/java/com/joabk/menus/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionMode mActionMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,56 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = (TextView)findViewById(R.id.tvTitle);
         this.registerForContextMenu(tv);
+
+        Button button = (Button)findViewById(R.id.btnActionMode);
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if(mActionMode !=null)
+                    return false;
+
+                mActionMode = startActionMode(mActionModeCallBack);
+                return false;
+            }
+        });
     }
+
+
+    private ActionMode.Callback mActionModeCallBack = new ActionMode.Callback(){
+
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.actionmode_context_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            switch(menuItem.getItemId()){
+                case R.id.actionmodemenu1:
+                    Toast.makeText(MainActivity.this, "Action Mode 1 clicked", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                case R.id.actionmodemenu2:
+                    Toast.makeText(getApplicationContext(), "Action Mode 2 clicked", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            mActionMode = null;
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,11 +96,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()){
             case R.id.menucontext1:
-                Toast.makeText(this,"Context menu 1 clicked", Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"Context menu 1 clicked", Toast.LENGTH_SHORT).show();
             case R.id.menucontext2:
-                Toast.makeText(this, "Context menu 2 clicked", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Context menu 2 clicked", Toast.LENGTH_SHORT).show();
             case R.id.menucontext3:
-                Toast.makeText(this, "Context menu 3 clicked", Toast.LENGTH_LONG);
+                Toast.makeText(this, "Context menu 3 clicked", Toast.LENGTH_SHORT).show();
             default:
                 return super.onContextItemSelected(item);
         }
