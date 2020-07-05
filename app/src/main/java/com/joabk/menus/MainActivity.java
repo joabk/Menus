b43b9988391767ecac4b4ea6d8a39eb7952eca64
Joab Karani <mjoab88@gmail.com>
2020-07-05 15:39:17 +0300

    Added and inflated context menu

--- app/src/main/java/com/joabk/menus/MainActivity.java
+++ app/src/main/java/com/joabk/menus/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView)findViewById(R.id.tvTitle);
+        this.registerForContextMenu(tv);
     }
 
     @Override
@@ -23,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.options_menu, menu);
         //return super.onCreateOptionsMenu(menu);
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.floating_context_menu, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menucontext1:
+                Toast.makeText(this,"Context menu 1 clicked", Toast.LENGTH_LONG).show();
+            case R.id.menucontext2:
+                Toast.makeText(this, "Context menu 2 clicked", Toast.LENGTH_LONG).show();
+            case R.id.menucontext3:
+                Toast.makeText(this, "Context menu 3 clicked", Toast.LENGTH_LONG);
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     @Override
